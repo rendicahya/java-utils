10ff2cb55aff64ce54e4b344da01f5d6a2c64186
Randy Cahya Wihandik <rendicahya@gmail.com>
2018-11-27 19:13:36 +0700

    Added a number of string classes and interfaces.

---
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/interfaces/StringArrayStringReducer.java
@@ -0,0 +1,6 @@
+package id.ac.ub.filkom.rendicahya.utils.string.interfaces;
+
+public interface StringArrayStringReducer {
+
+    String reduce(String[] input, String s);
+}
---
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/Reverser.java
@@ -0,0 +1,8 @@
+package id.ac.ub.filkom.rendicahya.utils.string.operators;
+
+import id.ac.ub.filkom.rendicahya.utils.string.interfaces.StringMapper;
+
+// Input: "foo bar"
+// Output: "rab oof"
+public class Reverser implements StringMapper {
+}

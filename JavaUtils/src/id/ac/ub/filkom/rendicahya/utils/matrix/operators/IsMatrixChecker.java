a1b61ab1efa583e8139a445d49316eae93cd609f
Panji Husni Padhila <Panji Husni Padhila@DESKTOP-7A52LD7>
2018-11-28 11:41:32 +0700

    this is a matrix checker

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/matrix/operators/IsMatrixChecker.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/matrix/operators/IsMatrixChecker.java
@@ -13,4 +13,77 @@ import id.ac.ub.filkom.rendicahya.utils.matrix.interfaces.MatrixPredicate;
 //         [4, 5, 6, 7]]
 // Output: false
 public class IsMatrixChecker implements MatrixPredicate {
+
+    @Override
+    public boolean test(byte[][] input) {
+        boolean cek = true;
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].length != input[0].length){
+                cek = false;
+                break;
+            } 
+        }
+        return cek;
+    }
+
+    @Override
+    public boolean test(short[][] input) {
+        boolean cek = true;
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].length != input[0].length){
+                cek = false;
+                break;
+            } 
+        }
+        return cek;
+    }
+
+    @Override
+    public boolean test(int[][] input) {
+        boolean cek = true;
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].length != input[0].length){
+                cek = false;
+                break;
+            } 
+        }
+        return cek;
+    }
+
+    @Override
+    public boolean test(long[][] input) {
+        boolean cek = true;
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].length != input[0].length){
+                cek = false;
+                break;
+            } 
+        }
+        return cek;
+    }
+
+    @Override
+    public boolean test(float[][] input) {
+        boolean cek = true;
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].length != input[0].length){
+                cek = false;
+                break;
+            } 
+        }
+        return cek;
+    }
+
+    @Override
+    public boolean test(double[][] input) {
+        boolean cek = true;
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].length != input[0].length){
+                cek = false;
+                break;
+            } 
+        }
+        return cek;
+    }
+   
 }

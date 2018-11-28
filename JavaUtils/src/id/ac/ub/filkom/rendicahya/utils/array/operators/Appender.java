796b88b2e0e389d5ca7aeebb440cb7f21239e2fe
Andrianov Lovanshandsky <ecxploreratlazcrew@gmail.com>
2018-11-29 01:14:43 +0700

    Added javadoc for each method.

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Appender.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Appender.java
@@ -6,9 +6,18 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.TwoArraysMapper;
 // Input 2: [4, 5, 6]
 // Output: [1, 2, 3, 4, 5, 6]
 public class Appender implements TwoArraysMapper {
+
+    /**
+     * Concatenate two arrays of BYTE. A new array will be created with length are
+     * combined from both arrays. Then do looping during length of new created
+     * array. Each array index filled into new array, first and second array
+     * continously. Next, the result will be return to the caller method.
+     * 
+     * @return Concatenated array of BYTE.
+     */
     @Override
     public byte[] operate(byte[] a, byte[] b) {
-        byte arrAppend = new byte[a.length + b.length];
+        var arrAppend = new byte[a.length + b.length];
 
         for (int i = 0; i < arrAppend.length; i++) {
             if (i < a.length)
@@ -20,9 +29,17 @@ public class Appender implements TwoArraysMapper {
         return arrAppend;
     }
 
+    /**
+     * Concatenate two arrays of DOUBLE. A new array will be created with length are
+     * combined from both arrays. Then do looping during length of new created
+     * array. Each array index filled into new array, first and second array
+     * continously. Next, the result will be return to the caller method.
+     * 
+     * @return Concatenated array of DOUBLE.
+     */
     @Override
     public double[] operate(double[] a, double[] b) {
-        double arrAppend = new double[a.length + b.length];
+        var arrAppend = new double[a.length + b.length];
 
         for (int i = 0; i < arrAppend.length; i++) {
             if (i < a.length)
@@ -34,9 +51,17 @@ public class Appender implements TwoArraysMapper {
         return arrAppend;
     }
 
+    /**
+     * Concatenate two arrays of FLOAT. A new array will be created with length are
+     * combined from both arrays. Then do looping during length of new created
+     * array. Each array index filled into new array, first and second array
+     * continously. Next, the result will be return to the caller method.
+     * 
+     * @return Concatenated array of FLOAT.
+     */
     @Override
     public float[] operate(float[] a, float[] b) {
-        float arrAppend = new float[a.length + b.length];
+        var arrAppend = new float[a.length + b.length];
 
         for (int i = 0; i < arrAppend.length; i++) {
             if (i < a.length)
@@ -48,9 +73,17 @@ public class Appender implements TwoArraysMapper {
         return arrAppend;
     }
 
+    /**
+     * Concatenate two arrays of INTEGER. A new array will be created with length
+     * are combined from both arrays. Then do looping during length of new created
+     * array. Each array index filled into new array, first and second array
+     * continously. Next, the result will be return to the caller method.
+     * 
+     * @return Concatenated array of INTEGER.
+     */
     @Override
     public int[] operate(int[] a, int[] b) {
-        int arrAppend = new int[a.length + b.length];
+        var arrAppend = new int[a.length + b.length];
 
         for (int i = 0; i < arrAppend.length; i++) {
             if (i < a.length)
@@ -62,9 +95,17 @@ public class Appender implements TwoArraysMapper {
         return arrAppend;
     }
 
+    /**
+     * Concatenate two arrays of LONG. A new array will be created with length are
+     * combined from both arrays. Then do looping during length of new created
+     * array. Each array index filled into new array, first and second array
+     * continously. Next, the result will be return to the caller method.
+     * 
+     * @return Concatenated array of LONG.
+     */
     @Override
     public long[] operate(long[] a, long[] b) {
-        long arrAppend = new long[a.length + b.length];
+        var arrAppend = new long[a.length + b.length];
 
         for (int i = 0; i < arrAppend.length; i++) {
             if (i < a.length)
@@ -76,9 +117,17 @@ public class Appender implements TwoArraysMapper {
         return arrAppend;
     }
 
+    /**
+     * Concatenate two arrays of SHORT. A new array will be created with length are
+     * combined from both arrays. Then do looping during length of new created
+     * array. Each array index filled into new array, first and second array
+     * continously. Next, the result will be return to the caller method.
+     * 
+     * @return Concatenated array of SHORT.
+     */
     @Override
     public short[] operate(short[] a, short[] b) {
-        short arrAppend = new short[a.length + b.length];
+        var arrAppend = new short[a.length + b.length];
 
         for (int i = 0; i < arrAppend.length; i++) {
             if (i < a.length)

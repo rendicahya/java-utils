95790b56d93d8d4a0ab3b89eea27b60707a6b0f2
Andrianov Lovanshandsky <ecxploreratlazcrew@gmail.com>
2018-11-28 22:20:58 +0700

    Implements interface of TwoArraysMapper

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Appender.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Appender.java
@@ -6,4 +6,33 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.TwoArraysMapper;
 // Input 2: [4, 5, 6]
 // Output: [1, 2, 3, 4, 5, 6]
 public class Appender implements TwoArraysMapper {
+    @Override
+    public byte[] operate(byte[] a, byte[] b) {
+        return null;
+    }
+
+    @Override
+    public double[] operate(double[] a, double[] b) {
+        return null;
+    }
+
+    @Override
+    public float[] operate(float[] a, float[] b) {
+        return null;
+    }
+
+    @Override
+    public int[] operate(int[] a, int[] b) {
+        return null;
+    }
+
+    @Override
+    public long[] operate(long[] a, long[] b) {
+        return null;
+    }
+
+    @Override
+    public short[] operate(short[] a, short[] b) {
+        return null;
+    }
 }

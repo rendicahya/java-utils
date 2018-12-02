8bb6e8f4975ca94600c85ba93dcaa964c01ec872
WINDOWS 10 <WINDOWS 10@Zil>
2018-12-02 21:27:32 +0700

    MatrixMaxFinder by Burhan Ilham Haqiqi

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/matrix/operators/MatrixMaxFinder.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/matrix/operators/MatrixMaxFinder.java
@@ -7,4 +7,17 @@ import id.ac.ub.filkom.rendicahya.utils.matrix.interfaces.MatrixReducer;
 //         [8, 5, 3]]
 // Output: 8
 public class MatrixMaxFinder implements MatrixReducer {
+    int [][]input ={{3, 1, 7},
+                 {5, 2, 2},
+                 {8, 5, 3}};
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < input.length; i++) {
+        for (int j = 0; j < input.length; j++) {
+            if (in[i][j]>max) {
+                max=in[i][j];
+            }
+        }
+    }
+    System.out.println("Output : "+max);
+
 }

1d75fe5eb936ea005bc2d4ec0946f0a35c8b6457
Andrianov Lovanshandsky <ecxploreratlazcrew@gmail.com>
2018-12-03 12:14:48 +0700

    penambahan kode untuk maxFinder

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/ArrayMaxFinder.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/ArrayMaxFinder.java
@@ -5,4 +5,80 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.ArrayReducer;
 // Input: [3, 1, 7]
 // Output: 7
 public class ArrayMaxFinder implements ArrayReducer {
+    @Override
+    public byte reduce(byte[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = input[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public double reduce(double[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = input[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public float reduce(float[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = input[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public int reduce(int[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = input[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public long reduce(long[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    public short reduce(short[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = input[i];
+            }
+        }
+
+        return max;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }

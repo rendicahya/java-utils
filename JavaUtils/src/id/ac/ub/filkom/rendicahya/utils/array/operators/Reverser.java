e79fdf75c8cc5698b3fabf2e2921cbc57185fd45
ASUS GAMING <ASUS GAMING@UltimateZee>
2018-12-04 16:03:59 +0700

    I have Created a Program to Reverse an Array

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Reverser.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Reverser.java
@@ -5,4 +5,70 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.ArrayMapper;
 // Input: [1, 2, 3, 4]
 // Output: [4, 3, 2, 1]
 public class Reverser implements ArrayMapper {
+
+    @Override
+    public byte[] map(byte[] a) {
+        byte []reverse = new byte[a.length];
+        for (int i = 0; i < a.length/2; i++)  { // for loop, that checks each array slot
+        reverse[i] = a[i];
+        a[i] = a[a.length-1-i];  // creates a new array that is in reverse order of the original
+        a[a.length-1-i] = reverse[i];
+        }
+        return reverse; 
+    }
+
+    @Override
+    public short[] map(short[] a) {
+        short []reverse = new short[a.length];
+        for (int i = 0; i < a.length/2; i++)  { // for loop, that checks each array slot
+        reverse[i] = a[i];
+        a[i] = a[a.length-1-i];  // creates a new array that is in reverse order of the original
+        a[a.length-1-i] = reverse[i];
+        }
+        return reverse;
+    }
+
+    @Override
+    public int[] map(int[] a) {
+        int []reverse = new int[a.length];
+        for (int i = 0; i < a.length/2; i++)  { // for loop, that checks each array slot
+        reverse[i] = a[i];
+        a[i] = a[a.length-1-i];  // creates a new array that is in reverse order of the original
+        a[a.length-1-i] = reverse[i];
+        }
+        return reverse;
+    }
+
+    @Override
+    public long[] map(long[] a) {
+        long []reverse = new long[a.length];
+        for (int i = 0; i < a.length/2; i++)  { // for loop, that checks each array slot
+        reverse[i] = a[i];
+        a[i] = a[a.length-1-i];  // creates a new array that is in reverse order of the original
+        a[a.length-1-i] = reverse[i];
+        }
+        return reverse;
+    }
+
+    @Override
+    public float[] map(float[] a) {
+        float []reverse = new float[a.length];
+        for (int i = 0; i < a.length/2; i++)  { // for loop, that checks each array slot
+        reverse[i] = a[i];
+        a[i] = a[a.length-1-i];  // creates a new array that is in reverse order of the original
+        a[a.length-1-i] = reverse[i];
+        }
+        return reverse;
+    }
+
+    @Override
+    public double[] map(double[] a) {
+        double []reverse = new double[a.length];
+        for (int i = 0; i < a.length/2; i++)  { // for loop, that checks each array slot
+        reverse[i] = a[i];
+        a[i] = a[a.length-1-i];  // creates a new array that is in reverse order of the original
+        a[a.length-1-i] = reverse[i];
+        }
+        return reverse;
+    }
 }

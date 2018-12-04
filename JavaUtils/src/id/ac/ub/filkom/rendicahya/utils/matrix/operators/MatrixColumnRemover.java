c5a502f3e50fe46c001bc88dc8aedc25a2742832
User <User@DESKTOP-FOO3AO2>
2018-12-04 09:23:51 +0700

    Matrix Column Remover - Kresna Hafizh Muhaimin

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/matrix/operators/MatrixColumnRemover.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/matrix/operators/MatrixColumnRemover.java
@@ -10,4 +10,106 @@ import id.ac.ub.filkom.rendicahya.utils.matrix.interfaces.MatrixIntMapper;
 //          [4, 5],
 //          [7, 8]
 public class MatrixColumnRemover implements MatrixIntMapper {
+
+    @Override
+    public byte[][] operate(byte[][] a, int b) {
+        byte[][] newArray = new byte[a.length][a[0].length - 1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (j < b - 1) {
+                    newArray[i][j] = a[i][j];
+                } else if (j == b - 1) {
+                    continue;
+                } else {
+                    newArray[i][j - 1] = a[i][j];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public short[][] operate(short[][] a, int b) {
+        short[][] newArray = new short[a.length][a[0].length - 1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (j < b - 1) {
+                    newArray[i][j] = a[i][j];
+                } else if (j == b - 1) {
+                    continue;
+                } else {
+                    newArray[i][j - 1] = a[i][j];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public int[][] operate(int[][] a, int b) {
+        int[][] newArray = new int[a.length][a[0].length - 1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (j < b - 1) {
+                    newArray[i][j] = a[i][j];
+                } else if (j == b - 1) {
+                    continue;
+                } else {
+                    newArray[i][j - 1] = a[i][j];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public long[][] operate(long[][] a, int b) {
+        long[][] newArray = new long[a.length][a[0].length - 1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (j < b - 1) {
+                    newArray[i][j] = a[i][j];
+                } else if (j == b - 1) {
+                    continue;
+                } else {
+                    newArray[i][j - 1] = a[i][j];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public float[][] operate(float[][] a, int b) {
+        float[][] newArray = new float[a.length][a[0].length - 1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (j < b - 1) {
+                    newArray[i][j] = a[i][j];
+                } else if (j == b - 1) {
+                    continue;
+                } else {
+                    newArray[i][j - 1] = a[i][j];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    @Override
+    public double[][] operate(double[][] a, int b) {
+        double[][] newArray = new double[a.length][a[0].length - 1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (j < b - 1) {
+                    newArray[i][j] = a[i][j];
+                } else if (j == b - 1) {
+                    continue;
+                } else {
+                    newArray[i][j - 1] = a[i][j];
+                }
+            }
+        }
+        return newArray;
+    }
 }

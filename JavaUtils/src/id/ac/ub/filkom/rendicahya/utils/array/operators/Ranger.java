b4f620418f4d4d39c639c3dbe87f0694ca8916ad
Global Store <Global Store@DESKTOP-7109Q5F>
2018-12-05 10:08:12 +0700

    Array Ranger

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Ranger.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Ranger.java
@@ -17,4 +17,48 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.OneTwoThreeIntMapper;
 // Input: 0, -10, 2
 // Output: [0, -2, -4, -6, -8, -9]
 public class Ranger implements OneTwoThreeIntMapper {
+
+    @Override
+    public int[] operate(int a) {
+        int[] output = new int[a];
+        int temp=0;
+        for (int i = 0; i < output.length; i++) {
+            output[i] = temp++;
+        }
+        return output;
+    }
+
+    @Override
+    public int[] operate(int a, int b) {
+        int[] output = new int[Math.abs(b - a)];
+        if (b - a > 0) {
+            for (int i = 0; i < output.length; i++) {
+                output[i] = a++;
+            }
+        } else {
+            for (int i = 0; i < output.length; i++) {
+                output[i] = a--;
+            }
+        }
+        return output;
+    }
+
+    @Override
+    public int[] operate(int a, int b, int c) {
+        int []output = new int[Math.abs(b-a)/c];
+        int temp = a;
+        output[0]=a;
+        if (b - a > 0) {
+            for (int i = 1; i < output.length; i++) {
+                temp += c; 
+                output[i] = temp;
+            }
+        } else {
+            for (int i = 1; i < output.length; i++) {
+                temp += c;
+                output[i] = temp;
+            }
+        }
+        return output;
+    }
 }

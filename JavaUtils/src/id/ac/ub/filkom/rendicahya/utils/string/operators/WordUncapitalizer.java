db12791fd8514097795566d3da1e1db65086a615
yunic <yunic@YUNICO-COMPUTER>
2018-12-05 20:36:59 +0700

    wordUncapitalizer by Yunico Ardian Pradana TIF-E

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/WordUncapitalizer.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/WordUncapitalizer.java
@@ -1,6 +1,7 @@
 package id.ac.ub.filkom.rendicahya.utils.string.operators;
 
 import id.ac.ub.filkom.rendicahya.utils.string.interfaces.StringMapper;
+import java.util.Scanner;
 
 // Input: "Foo Bar"
 // Output: "foo bar"
@@ -8,4 +9,21 @@ import id.ac.ub.filkom.rendicahya.utils.string.interfaces.StringMapper;
 // Input: "foo Bar"
 // Output: "foo bar"
 public class WordUncapitalizer implements StringMapper {
+ 
+    public static String worduncapitalizer (String input) {
+        String output = input.toLowerCase().trim();
+        return output;
+    }
+
+    @Override
+    public String map(String input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.println("Masukkan huruf");
+        String masuk = in.nextLine();
+        System.out.println(worduncapitalizer(masuk));
+    }
 }
+   

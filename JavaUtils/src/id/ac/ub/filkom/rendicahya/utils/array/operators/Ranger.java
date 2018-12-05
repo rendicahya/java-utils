10f39117cbcc31471d39c1c5dd20faf50ee66042
Global Store <Global Store@DESKTOP-7109Q5F>
2018-12-05 10:14:21 +0700

    Array Ranger

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Ranger.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/Ranger.java
@@ -1,7 +1,12 @@
 package id.ac.ub.filkom.rendicahya.utils.array.operators;
 
 import id.ac.ub.filkom.rendicahya.utils.array.interfaces.OneTwoThreeIntMapper;
-
+/** 
+ * Array Ranger
+ * to create an array which has starting point, finishing point, and gap between each numbers
+ * 
+ * @author Ahya Zhilalikbar Amin 
+ */
 // Input: 10
 // Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 

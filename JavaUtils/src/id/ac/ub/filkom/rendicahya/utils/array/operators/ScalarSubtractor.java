37ba862ad924dea065b24054eb1fbebb5a519a41
Annisah Amalia <>
2018-12-05 13:10:20 +0700

    Scalar Substractor by Annisah Amalia

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/ScalarSubtractor.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/ScalarSubtractor.java
@@ -6,4 +6,58 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.ArrayScalarMapper;
 // Input 2: 3
 // Output: [-2, -1, 0]
 public class ScalarSubtractor implements ArrayScalarMapper {
+
+    @Override
+    public byte[] operate(byte[] a, byte b) {
+        byte[] c = new byte[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = (byte) (a[i] - b);
+        }
+        return c;
+    }
+
+    @Override
+    public short[] operate(short[] a, short b) {
+        short[] c = new short[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = (short) (a[i] - b);
+        }
+        return c;
+    }
+
+    @Override
+    public int[] operate(int[] a, int b) {
+        int[] c = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] - b;
+        }
+        return c;
+    }
+
+    @Override
+    public long[] operate(long[] a, long b) {
+        long[] c = new long[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] - b;
+        }
+        return c;
+    }
+
+    @Override
+    public float[] operate(float[] a, float b) {
+        float[] c = new float[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] - b;
+        }
+        return c;
+    }
+
+    @Override
+    public double[] operate(double[] a, double b) {
+        double[] c = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] - b;
+        }
+        return c;
+    }
 }

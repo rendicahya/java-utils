12a4234131c6b4fbe50cd49e9a96393d4acf9a41
AlfirsaFA <>
2018-12-05 10:02:34 +0700

    Right Trim Method by Alfirsa Damasyifa Fauzulhaq

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/RightTrim.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/RightTrim.java
@@ -7,11 +7,12 @@ import id.ac.ub.filkom.rendicahya.utils.string.interfaces.StringMapper;
 public class RightTrim implements StringMapper {    
     @Override
     public String map(String input) {
-        int i = input.length() - 1;
-        while (i >= 0 && Character.isWhitespace(input.charAt(i))) {
-            i--;
+        int i = input.length() - 1;                                                // banyak (huruf - 1) untuk substring
+        while (i >= 0 && Character.isWhitespace(input.charAt(i))) {                // pengecekan whitespace dari kanan string
+            i--;                                                                   // mengurangi i untuk menghapus whitespace
         }
-        return input.substring(0, i + 1);
+        return input.substring(0, i + 1);                                          // mengembalikan nilai input dari huruf ke 0 sampai huruf ke i + 1 (dari awal)
+        
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

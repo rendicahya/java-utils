8193f49e2e1cb0cb240ccfbe4db7259e24db5cef
gloriafriska <45345709+gloriafriska@users.noreply.github.com>
2018-12-05 19:08:51 +0700

    Finishing Scalar Divider by Gloria Friska

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/ScalarDivider.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/ScalarDivider.java
@@ -6,4 +6,63 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.ArrayIntToDoubleArrayMa
 // Input 2: 2
 // Output: [0.5, 1, 1.5]
 public class ScalarDivider implements ArrayIntToDoubleArrayMapper {
+  @Override
+    public double[] operate(byte[] a, byte b) {
+        double []divide = new double [a.length];
+        for (int i = 0; i < a.length; i++) {
+            divide[i]=a[i];
+            divide[i]/=b;  
+        }
+        return divide;
+    }
+
+    @Override
+    public double[] operate(short[] a, short b) {
+        double[]divide=new double [a.length];
+        for (int i = 0; i < a.length; i++) {
+            divide [i]=a[i];
+            divide [i]/=b;
+        }
+        return divide;
+    }
+
+    @Override
+    public double[] operate(int[] a, int b) {
+        double[]divide=new double [a.length];
+        for (int i = 0; i < a.length; i++) {
+            divide [i]=a[i];
+            divide [i]/=b;
+        }
+        return divide;
+    }
+
+    @Override
+    public double[] operate(long[] a, long b) {
+        double []divide = new double [a.length];
+        for (int i = 0; i < a.length; i++) {
+            divide[i]=a[i];
+            divide[i]/=a[i];
+        }
+        return divide;
+    }
+
+    @Override
+    public double[] operate(float[] a, float b) {
+        double []divide = new double [a.length];
+        for (int i = 0; i < a.length; i++) {
+            divide[i]=a[i];
+            divide[i]/=a[i];
+        }
+        return divide;
+    }
+
+    @Override
+    public double[] operate(double[] a, double b) {
+        double []divide = new double [a.length];
+        for (int i = 0; i < a.length; i++) {
+            divide[i]=a[i];
+            divide[i]/=a[i];
+        }
+        return divide;
+    }
 }

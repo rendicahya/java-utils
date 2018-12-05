1ed2cc7783eec78a27bb4ba9e522891cb46a986f
AlfirsaFA <>
2018-12-05 09:28:30 +0700

    Right Trim Method by Alfirsa Damasyifa Fauzulhaq

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/RightTrim.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/RightTrim.java
@@ -4,5 +4,14 @@ import id.ac.ub.filkom.rendicahya.utils.string.interfaces.StringMapper;
 
 // Input: "  foo bar   "
 // Output: "  foo bar"
-public class RightTrim implements StringMapper {
+public class RightTrim implements StringMapper {    
+    @Override
+    public String map(String input) {
+        int i = input.length() - 1;
+        while (i >= 0 && Character.isWhitespace(input.charAt(i))) {
+            i--;
+        }
+        return input.substring(0, i + 1);
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }

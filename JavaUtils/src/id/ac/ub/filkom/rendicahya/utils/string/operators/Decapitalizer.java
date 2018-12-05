61c61a6ff12ad116fcdc6a093e58616edce5d85a
Ny  07 <Ny  07@DESKTOP-LP2QBN2.local>
2018-12-05 20:32:19 +0700

    menjadikan huruf kecil huruf pertama dari kata pertama dari input

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/Decapitalizer.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/string/operators/Decapitalizer.java
@@ -5,4 +5,16 @@ import id.ac.ub.filkom.rendicahya.utils.string.interfaces.StringMapper;
 // Input: "Foo Bar"
 // Output: "foo Bar"
 public class Decapitalizer implements StringMapper {
+
+    @Override
+    public String map(String input) {
+        char[] pisah = new char[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            pisah[i] = input.charAt(i);
+            pisah[0] = Character.toLowerCase(pisah[0]);
+        }
+
+        return String.valueOf(pisah);
+    }
+
 }

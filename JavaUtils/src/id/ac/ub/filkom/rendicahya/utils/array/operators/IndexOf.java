74c3437ecf27e388b0145807b37b2fd2b58c2ce4
Hensel <Hensel@DESKTOP-9TDL55S.local>
2018-12-05 14:29:53 +0700

    Getting an Array Input and return the value of the specific index based on the input

--- JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/IndexOf.java
+++ JavaUtils/src/id/ac/ub/filkom/rendicahya/utils/array/operators/IndexOf.java
@@ -6,4 +6,76 @@ import id.ac.ub.filkom.rendicahya.utils.array.interfaces.ArrayIntToIntReducer;
 // Input 2: 5
 // Output: 2
 public class IndexOf implements ArrayIntToIntReducer {
+
+    @Override
+    public int reduce(byte[] a, int b) {
+        int Output = -1;
+        for (int i = 0; i < a.length; ++i) {
+            if (b == a[i]) {
+                Output = i;
+                break;
+            }
+        }
+        return Output;
+    }
+
+    @Override
+    public int reduce(short[] a, int b) {
+        int Output = -1;
+        for (int i = 0; i < a.length; ++i) {
+            if (b == a[i]) {
+                Output = i;
+                break;
+            }
+        }
+        return Output;
+    }
+
+    @Override
+    public int reduce(int[] a, int b) {
+        int Output = -1;
+        for (int i = 0; i < a.length; ++i) {
+            if (b == a[i]) {
+                Output = i;
+                break;
+            }
+        }
+        return Output;
+    }
+
+    @Override
+    public int reduce(long[] a, int b) {
+        int Output = -1;
+        for (int i = 0; i < a.length; ++i) {
+            if (b == a[i]) {
+                Output = i;
+                break;
+            }
+        }
+        return Output;
+    }
+
+    @Override
+    public int reduce(float[] a, int b) {
+        int Output = -1;
+        for (int i = 0; i < a.length; ++i) {
+            if (b == a[i]) {
+                Output = i;
+                break;
+            }
+        }
+        return Output;
+    }
+
+    @Override
+    public int reduce(double[] a, int b) {
+        int Output = -1;
+        for (int i = 0; i < a.length; ++i) {
+            if (b == a[i]) {
+                Output = i;
+                break;
+            }
+        }
+        return Output;
+    }
 }
